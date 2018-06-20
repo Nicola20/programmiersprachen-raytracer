fa77983d8e26091f0a23d070e1b24f72a1e9deb9
Nicola20 <nicola.lea.libera@uni-weimar.de>
2018-06-20 16:19:09 +0200

    changed constructors

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -1,3 +1,9 @@
-class Shape {
-    
-}
+#include "shape.hpp"
+
+Shape::Shape():
+    name_{"DefaultShape"},
+    color_{0.0f,0.0f,0.0f} {}
+
+Shape::Shape(std::string const& name, Color const& col):
+    name_{name},
+    color_{col}{}
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -1,10 +1,19 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
 
+#include "color.hpp"
+#include <string>
+
 class Shape {
 
     public:
     virtual double area() const = 0;  //calculates the surface
     virtual double volume() const = 0;  //calculates the volume
+    Shape();
+    Shape(std::string const& name, Color const& col);
+
+    protected:
+    std::string name_;
+    Color color_;
 };
 #endif // SHAPE_HPP
