eb365effe5a489305985a025e9d79bc74b20a58c
Nicola20 <nicola.lea.libera@uni-weimar.de>
2018-07-04 12:55:21 +0200

    not quite working

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -2,13 +2,19 @@
 
 Shape::Shape():
     name_{"DefaultShape"},
-    color_{0.0f,0.0f,0.0f} {
+    color_{1.0f, 1.0f, 1.0f} {
         std::cout << "ctor of base class shape \n";
     }
 
-Shape::Shape(std::string const& name, Color const& col):
+Shape::Shape(std::string const& name, Color const& col): //hier dann einfach col.name etc. in den Konstruktor
     name_{name},
     color_{col}{
+        std::cout << "ctor of base class shape \n";
+    }
+
+Shape(std::string const& name, std::shared_ptr<Material> const& material):
+    name_{name},
+    material_{material} {
         std::cout << "ctor of base class shape \n";
     }
 
