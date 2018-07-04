fa3ee5b1b133233e38bf07bedfdaa1d882c9fda6
Nicola20 <nicola.lea.libera@uni-weimar.de>
2018-07-04 12:54:44 +0200

    bis zur 6.4 aber 6.4 funzt noch nicht

--- framework/box.cpp
+++ framework/box.cpp
@@ -15,6 +15,13 @@ Box::Box (glm::vec3 const& min, glm::vec3 const& max,std::string const& name, Co
         std::cout << "ctor of derived class Box \n";
     }
 
+Box (glm::vec3 const& min, glm::vec3 const& max,std::string const& name, std::shared_ptr<Material> const& material):
+    Shape::Shape(name, material),
+    min_{min},
+    max_{max} {
+        std::cout << "ctor of derived class Box \n";
+    }
+
 Box::~Box(){
     std::cout << "dtor of derived class Box \n";
 }
@@ -52,3 +59,37 @@ std::ostream& Box::print(std::ostream& os) const {
     os << "Vector max_: (" << max_.x << "," << max_.y << "," << max_.z << "), \n" << "Vector min_: (" << min_.x << "," << min_.y << "," << min_.z << ") \n \n";
     return os;
 }
+
+bool Box::intersect (Ray const& ray, float& t) const {  
+    float tx1 = (min_.x - ray.origin_.x)/ray.direction_.x;
+    float tx2 = (max_.x - ray.origin_.x)/ray.direction_.x;
+
+    float ty1 = (min_.y - ray.origin_.y)/ray.direction_.y;
+    float ty2 = (max_.y - ray.origin_.y)/ray.direction_.y;
+
+    float tz1 = (min_.z - ray.origin_.z)/ray.direction_.z;
+    float tz2 = (max_.z - ray.origin_.z)/ray.direction_.z;
+
+    float tfarx = std::max(tx1, tx2);
+    float tnearx = std::min(tx1, tx2);
+
+    float tfary = std::max(ty1, ty2);  //warum wird hier max und min anders herum verwendet?
+    float tneary = std::min(ty1, ty2);
+
+    float tfar = std::min(tfarx, tfary);
+    float tnear = std::max(tnearx, tneary);
+
+    if(tfar < tnear){
+        return false;
+    }
+
+    tfar = std::min(tz2, tfar);
+    tnear = std::max(tz1, tnear);       //tnear ist schnittpunkt
+
+    if(tfar < tnear){
+        return false;
+    }
+    //Eigentlichen Schnittpunkt berechnen
+     float distance = std::abs(tnear);
+    return true;
+} 
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -2,6 +2,8 @@
 #define SHAPE_HPP
 
 #include "color.hpp"
+#include "ray.hpp"
+#include "material.hpp"
 #include <string>
 
 class Shape {
@@ -10,15 +12,18 @@ class Shape {
     virtual double area() const = 0;  //calculates the surface
     virtual double volume() const = 0;  //calculates the volume
     Shape();
+    Shape(std::string const& name, std::shared_ptr<Material> const& material);
     Shape(std::string const& name, Color const& col);
     virtual std::ostream& print(std::ostream& os) const;
     std::string getName() const;
     Color getColor() const;
     virtual ~Shape();
     //~Shape();
+    virtual bool intersect (Ray const& ray, float& t) const = 0;
 
     protected:
     std::string name_;
+    std::shared_ptr<Material> material_; 
     Color color_;
 };
 
